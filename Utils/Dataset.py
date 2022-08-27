23422c0d66a06eb94251baf5835899bad2f29ffc
HIMANSHU SHARMA <hsmcaju@gmail.com>
2022-08-27 23:11:37 +0530

    Commit 4: Implemented the functionality for loading the training images and creating their dataset.

--- Utils/Dataset.py
+++ Utils/Dataset.py
@@ -1,4 +1,5 @@
-import numpy as np
+import os.path
+import tensorflow as tf
 
 
 class Dataset:
@@ -9,11 +10,49 @@ class Dataset:
 # This method returns a list of testing and training samples
 def split_dataset(image_names):
     # Randomize the data
-    np.random.shuffle(image_names)
-
+    tf.random.shuffle(image_names)
     # Find the total number of images and split the data into the ratio of 80:20 for training and testing
     data_length = len(image_names)
     # Use 80% of images for training
     training_data_size = int(data_length * 0.8)
 
     return image_names[:training_data_size], image_names[training_data_size:]
+
+
+# This method loads the list of images into the main memory and returns both the datasets namely training_image_names and training_images
+def load_images(directory_name, training_image_names, image_encoder):
+    # Read the input specifications of the image encoder
+    _, x, y, channels = image_encoder.input_shape
+
+    # Create a tf.dataset of the training_image_names for loading the actual images into main memory
+    training_image_names = tf.data.TextLineDataset.from_tensor_slices(training_image_names)
+
+    # Now, map this training_image_name tf.dataset with the actual images and generate their new tf.dataset as well
+    training_images = list(map(lambda image_name: load_image(directory_name, image_name.decode(), x, y, channels), training_image_names.as_numpy_iterator()))
+    training_image_dataset = tf.data.Dataset.from_tensor_slices(training_images)
+
+    # Once both the datasets have been prepared, return the dataset
+    return training_image_names, training_image_dataset
+
+
+# This method loads the image referenced by its path into the main memory
+def load_image(directory_name, image_name, x, y, channels):
+    # Generate the absolute image path
+    absolute_image_path = os.path.join(directory_name, str(image_name))
+
+    # Read the image file into the main memory
+    image_file = tf.io.read_file(absolute_image_path)
+
+    # Convert the image file into tensors of three RGB channel format
+    image = tf.image.decode_image(image_file, channels=channels)
+
+    # Resize the image to the
+    return tf.image.resize(image, (x, y))
+
+
+# This method extracts the features of the training images using the image encoder model
+def extract_features(image_encoder, training_images):
+    feature_list = list(training_images.map(lambda image: tf.keras.applications.inception_v3.preprocess_input(image)))
+
+    # Create a tf.Dataset for the feature list and return
+    return tf.data.Dataset.from_tensor_slices(feature_list)
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import os
 from Utils.CaptionLoader import load_captions, process_captions
-from Utils.Dataset import split_dataset
+from Utils.Dataset import split_dataset, load_images, extract_features
 from Utils.ImageEncoder import generate_inception_image_encoder
 
 # 1. PREPROCESS THE CAPTION DATASET
 # The address of the caption file that contains all the captions associated with the dataset
 
 
-caption_filepath = os.path.abspath('Data\Flickr_8K\captions.txt')
+caption_file_path = os.path.abspath('D:\modified_neuraltalk_master_tf\Data\Flickr_8K\captions.txt')
+image_directory_path = os.path.abspath('D:\modified_neuraltalk_master_tf\Data\Flickr_8K\Images')
 
 # Read the captions of the flickr 8k dataset into the main memory
 # The captions contains labeled dataset where each image id is associated with five captions. See example below
 # (1000268201_693b08cb0e.jpg,A child in a pink dress is climbing up a set of stairs in an entry way .)
-unprocessed_image_captions = load_captions(caption_filepath)
+unprocessed_image_captions = load_captions(caption_file_path)
 
 # The captions file as read above is unpacked version as every caption has its own image identifier
 # Now, we associate five captions of single image with it using a dictionary where the structure will be as follows
@@ -20,11 +21,13 @@ unprocessed_image_captions = load_captions(caption_filepath)
 processed_image_captions = process_captions(unprocessed_image_captions)
 
 # Create training and testing image split
-training_images, testing_images = split_dataset(list(processed_image_captions.keys()))
+training_image_names, testing_image_names = split_dataset(list(processed_image_captions.keys()))
 
-# 2. PREPROCESS THE IMAGES
+# 2. PREPROCESS THE IMAGES (FEATURE EXTRACTION)
 # Generate the image encoding model
 image_encoder = generate_inception_image_encoder()
 
+# Load the training images into the main memory of the predefined input size
+training_image_names, training_image_dataset = load_images(image_directory_path, training_image_names, image_encoder)
 
 
