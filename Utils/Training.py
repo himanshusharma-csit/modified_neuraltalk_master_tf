f02055807621ffd3b18125b905b34b0108d23539
HIMANSHU SHARMA <hsmcaju@gmail.com>
2022-09-27 01:28:02 +0530

    Commit 17: Pushing the Training.py as it was left in the last push

--- Utils/Training.py
+++ Utils/Training.py
@@ -1,16 +1,22 @@
 import time
+import absl.logging
 from tqdm import tqdm
 import tensorflow as tf
-
 from Utils.ModelDetails import training_step
 
+# To avoid displaying the save format warning everytime, we, verbose the logging here
+absl.logging.set_verbosity(absl.logging.ERROR)
 
 class Training:
     def __init__(self):
         print(">>> Training instance initialized...")
 
 
-def initialize_pipeline_training(image_captioning_dataset, feature_encoder, decoder, text_vectorizer, model_manager):
+def initialize_pipeline_training(image_captioning_dataset=None,
+                                 feature_encoder=None,
+                                 decoder=None,
+                                 text_vectorizer=None,
+                                 model_manager=None):
     loss_plot = []
     start_epoch = 0
     EPOCHS = 5
@@ -25,28 +31,26 @@ def initialize_pipeline_training(image_captioning_dataset, feature_encoder, deco
         for (batch, (img_tensor, target)) in tqdm(tf.data.Dataset.enumerate(image_captioning_dataset), ncols=100):
             # For now target is (64,1,max_size), so we squash the 1 dimension.
             # This is a bit of hack... not happy about it.
-            batch_loss, t_loss = training_step(img_tensor, tf.squeeze(target), feature_encoder, decoder,
-                                               text_vectorizer, model_manager)
+            batch_loss, t_loss = training_step(image_tensor=img_tensor,
+                                               target=tf.squeeze(target),
+                                               feature_encoder=feature_encoder,
+                                               decoder=decoder,
+                                               tokenizer=text_vectorizer,
+                                               model_manager=model_manager)
+            # Add the batch loss to the overall total loss
             total_loss += t_loss
-
-            # if (batch.numpy() % 20) == 0:
-            #     average_batch_loss = batch_loss.numpy() / int(target.shape[2])
-            #     print(f'\nEpoch {epoch + 1} Batch {batch} Loss {average_batch_loss:.4f}')
-
+        # Save the encoder and decoder model states after every epoch
+        feature_encoder.save(filepath=model_manager.encoder_path, overwrite=True, save_format="tf")
+        decoder.save(filepath=model_manager.decoder_path, overwrite=True, save_format="tf")
+        # Print the end epoch statistics
         epoch_end_statistics(epoch, start, total_loss / num_steps)
-
-        # Epoch has been completed, now save the weights of the model...  to prevent any failure during training
-        save_pipeline_weights(model_details=model_manager, feature_encoder=feature_encoder, decoder=decoder)
-
-    # storing the epoch end loss value to plot later
+    # Storing the epoch end loss value to plot later
     loss_plot.append(total_loss / num_steps)
-
-    # print(f'Epoch {epoch + 1} Loss {total_loss / num_steps:.6f}')
-    # print(f'Time taken for 1 epoch {time.time() - start:.2f} sec\n')
-
+    # Return the loss_plot values to the main
     return loss_plot
 
 
+# Creates a Model training initializer header
 def model_training_message():
     print(
         '\n\n********************************************************************************************************************')
@@ -55,37 +59,23 @@ def model_training_message():
         '********************************************************************************************************************')
 
 
+# Creates a start epoch header
 def epoch_start_statistics(epoch, epoch_start_time):
-    print(
-        '********************************************************************************************************************')
-    print('\n#######################################################')
+    print('\n********************************************************************************************************************')
+    print('\n####################################################################################################################')
     print('>>>> EPOCH NUMBER:', epoch, '\nSUCCESSFULLY INITIALIZED AT:', time.ctime(epoch_start_time))
-    print('#######################################################')
+    print('#####################################################################################################################')
     print(
         '------------------------------------------------------------------------------------------------------------------')
 
 
+# Creates an end epoch header
 def epoch_end_statistics(epoch, epoch_start_time, epoch_error):
     epoch_end_time = time.time()
     print(
-        '********************************************************************************************************************')
-    print(
-        '------------------------------------------------------------------------------------------------------------------')
+        '\n------------------------------------------------------------------------------------------------------------------')
     print('>>>> EPOCH NUMBER:', epoch, '\nSUCCESSFULLY COMPLETED AT:',
           time.ctime(epoch_end_time), 'IN',
           (epoch_end_time - epoch_start_time), 'seconds', '\n >>>TOTAL LOSS: ', epoch_error.numpy(), )
     print(
         '------------------------------------------------------------------------------------------------------------------')
-    print(
-        '********************************************************************************************************************')
-
-
-def save_pipeline_weights(model_details, feature_encoder, decoder):
-    feature_encoder.save_weights(model_details.encoder_path)
-    decoder.save_weights(model_details.decoder_path)
-
-
-def load_pipeline_weights(model_details, feature_encoder, decoder):
-    feature_encoder.load_weights('D:\modified_neuraltalk_master_tf\FeatureEncoderCheckpoints\Encoder.ckpt')
-    decoder.load_weights('D:\modified_neuraltalk_master_tf\DecoderCheckpoints\Decoder.ckpt')
-    return feature_encoder, decoder
